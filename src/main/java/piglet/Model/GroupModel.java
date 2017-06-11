a795c2bea81ef630065e0285865945dd59fefa00
Uprzejmy <lmerynda@gmail.com>
2017-06-12 01:19:18 +0200

    quickfix missing file

---
+++ src/main/java/piglet/Model/GroupModel.java
@@ -0,0 +1,49 @@
+package piglet.Model;
+
+
+import piglet.Model.Entity.Group;
+import piglet.View.IObserver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Uprzejmy on 12.06.2017.
+ */
+public class GroupModel {
+
+    private Collection<Group> groups;
+    Collection<IObserver> groupsModelObservers;
+
+    public GroupModel()
+    {
+        groups = new ArrayList<>();
+        groupsModelObservers = new ArrayList<>();
+
+    }
+
+    public Collection<Group> getGroups()
+    {
+        return groups;
+    }
+
+    public void addGroup(String name)
+    {
+        groups.add(new Group(name));
+
+        notifyGroupsModelObservers();
+    }
+
+    public void registerGroupsModelObserver(IObserver observer)
+    {
+        groupsModelObservers.add(observer);
+    }
+
+    private void notifyGroupsModelObservers()
+    {
+        for(IObserver observer : groupsModelObservers)
+        {
+            observer.update();
+        }
+    }
+}
