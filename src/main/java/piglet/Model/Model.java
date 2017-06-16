fc80b60510f8a3e383fd56685e4d72d8256dfb98
Uprzejmy <lmerynda@gmail.com>
2017-06-16 10:52:47 +0200

    separating example parts from model code

--- src/main/java/piglet/Model/Model.java
+++ src/main/java/piglet/Model/Model.java
@@ -1,6 +1,7 @@
 package piglet.Model;
 
 import piglet.Model.Entity.*;
+import piglet.Model.Utils.ExampleLoader;
 
 /**
  * Created by Uprzejmy on 11.06.2017.
@@ -18,7 +19,7 @@ public class Model {
         groupsModel = new GroupsModel();
         repositoriesModel = new RepositoriesModel();
 
-        loadExampleData();
+        ExampleLoader.loadExampleData(this);
     }
 
     public static Model getInstance()
@@ -52,60 +53,5 @@ public class Model {
     {
         return repositoriesModel;
     }
-
-    private void loadExampleData()
-    {
-        for(int i=0;i<30;i++)
-        {
-            usersModel.getUsers().add(new User("user" + i,"example public key" + i));
-        }
-
-        for(int i=0;i<10;i++)
-        {
-            Group group = new Group("test group" + i);
-
-            for(User user : usersModel.getUsers())
-            {
-                if(Math.random() < 0.2)
-                {
-                    group.addUser(user);
-                }
-            }
-
-            groupsModel.getGroups().add(group);
-        }
-
-        for(int i=0;i<3;i++)
-        {
-            Repository repository = new Repository("test repo" + i);
-
-            for(Group group : groupsModel.getGroups())
-            {
-                double result = Math.random();
-
-                if(Math.random() < 0.1)
-                {
-                    repository.addRepositoryPermission(group, EPermission.READWRITE);
-                }
-                else if(Math.random() < 0.3)
-                {
-                    repository.addRepositoryPermission(group, EPermission.READ);
-                }
-            }
-
-            for(User user : usersModel.getUsers())
-            {
-                if(Math.random() < 0.01)
-                {
-                    repository.addRepositoryPermission(user, EPermission.READWRITE);
-                }
-            }
-
-            //adding super user to the repo
-            int superUserIndex = (int) (Math.random() * usersModel.getUsers().size());
-            repository.addRepositoryPermission((User)usersModel.getUsers().toArray()[superUserIndex],EPermission.READWRITEADMIN);
-
-            repositoriesModel.getRepositories().add(repository);
-        }
-    }
+    
 }
---
+++ src/main/java/piglet/Model/Utils/ExampleLoader.java
@@ -0,0 +1,69 @@
+package piglet.Model.Utils;
+
+import piglet.Model.Entity.EPermission;
+import piglet.Model.Entity.Group;
+import piglet.Model.Entity.Repository;
+import piglet.Model.Entity.User;
+import piglet.Model.Model;
+
+/**
+ * Created by Uprzejmy on 16.06.2017.
+ */
+public class ExampleLoader {
+    public static void loadExampleData(Model model)
+    {
+        for(int i=0;i<30;i++)
+        {
+            model.getUsersModel().getUsers().add(new User("user" + i,"example public key" + i));
+        }
+
+        for(int i=0;i<10;i++)
+        {
+            Group group = new Group("test group" + i);
+
+            for(User user : model.getUsersModel().getUsers())
+            {
+                if(Math.random() < 0.2)
+                {
+                    group.addUser(user);
+                }
+            }
+
+            model.getGroupsModel().getGroups().add(group);
+        }
+
+        for(int i=0;i<3;i++)
+        {
+            Repository repository = new Repository("test repo" + i);
+
+            for(Group group : model.getGroupsModel().getGroups())
+            {
+                double result = Math.random();
+
+                if(Math.random() < 0.1)
+                {
+                    repository.addRepositoryPermission(group, EPermission.READWRITE);
+                }
+                else if(Math.random() < 0.3)
+                {
+                    repository.addRepositoryPermission(group, EPermission.READ);
+                }
+            }
+
+            for(User user : model.getUsersModel().getUsers())
+            {
+                if(Math.random() < 0.01)
+                {
+                    repository.addRepositoryPermission(user, EPermission.READWRITE);
+                }
+            }
+
+            //adding super user to the repo
+            int superUserIndex = (int) (Math.random() * model.getUsersModel().getUsers().size());
+            repository.addRepositoryPermission((User)model.getUsersModel().getUsers().toArray()[superUserIndex],EPermission.READWRITEADMIN);
+
+            model.getRepositoriesModel().getRepositories().add(repository);
+        }
+    }
+
+}
